f2e288ef3ed41bd2329c630248f0adcd01804392
Tiago de Lima <tiago.l07@aluno.ifsc.edu.br>
2022-03-27 21:15:53 -0300

    Edited recipe description and config

--- project/urls.py
+++ project/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from recipes.views import home
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', include('recipes.urls'),)
 ]
+
